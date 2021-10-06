536afd955164a3f1b6f0b030f3715bb12f540b82
Min Xuan <hi@ngminxuan.com>
2021-10-06 17:09:20 +0800

    formatting cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,6 @@ class HomeController < ApplicationController
     @posts = Post.all.includes(:user)
     @grouped_posts = @posts.group_by { |t| t.created_at.to_date == DateTime.now.to_date }
 
-    if @grouped_posts[false].present?
-      @posts_by_month = @grouped_posts[false].group_by { |t| t.created_at.month }
-    end
+    @posts_by_month = @grouped_posts[false].group_by { |t| t.created_at.month } if @grouped_posts[false].present?
   end
 end
