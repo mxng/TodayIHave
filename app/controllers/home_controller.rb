81c409e23ea55aa16a0384313baf357a7ca08eb8
Min Xuan <hi@ngminxuan.com>
2021-10-06 17:08:16 +0800

    made modal for new updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @post = Post.new
     @posts = Post.all.includes(:user)
     @grouped_posts = @posts.group_by { |t| t.created_at.to_date == DateTime.now.to_date }
 
