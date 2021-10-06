ba76154df81935e80431aefd6570a6a46169ba33
Min Xuan <hi@ngminxuan.com>
2021-10-06 15:38:38 +0800

    added grouping to post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
 
   def index
     @posts = Post.all.includes(:user)
+    @grouped_posts = @posts.group_by { |t| t.created_at.to_date == DateTime.now.to_date }
+
+    if @grouped_posts[false].present?
+      @posts_by_month = @grouped_posts[false].group_by { |t| t.created_at.month }
+    end
   end
 end
